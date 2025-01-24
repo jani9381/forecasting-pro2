9b4ec317f65e12b98bea206878e82fdb4a42bee4
Jani basha shaik <118528876+jani9381@users.noreply.github.com>
2025-01-24 22:21:12 +0530

    pro2.py

--- pro2.py
+++ pro2.py
@@ -10,6 +10,7 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 import warnings
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima.model import ARIMA
 from pmdarima import auto_arima
 from sklearn.preprocessing import MinMaxScaler
 from statsmodels.tsa.seasonal import seasonal_decompose
